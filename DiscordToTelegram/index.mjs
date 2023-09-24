ee4ada6b7b63b33362c491a848b90fbdaf6be69d
Shayan Noruzi <78757056+ShawnCentra@users.noreply.github.com>
2023-09-24 16:28:29 -0700

    issue with Oauth2

--- DiscordToTelegram/index.mjs
+++ DiscordToTelegram/index.mjs
@@ -4,18 +4,23 @@ import fetch from "node-fetch";
 import Bottleneck from "bottleneck";
 
 // Discord and Telegram setup
-let lastMessageId = null; // To keep track of the last message checked
-const DISCORD_AUTH =
-  "NDY1OTg1NjY4NDU2MzgyNDY3.GZXfGn.hNjBt96UkHK6H9VqheVsr4KABt8UrzZXrGCEXM"; // Not the bot token
-// const CHANNEL_ID = "1088103385674285066"; //infinintyAI
-const CHANNEL_ID = "1107797248676483144"; //luca ch id
-const TELEGRAM_TOKEN = "6640266877:AAH0-fiYqVBCcTM5PrbIWEBYNuEUeAg0COo";
-const TELEGRAM_CHAT_ID = "1611894170";
+let DISCORD_AUTH = process.env.DISCORD_AUTH || null;
+const CLIENT_ID = '1151989619018186833';
+const CLIENT_SECRET = 'atTul3Mxw9dfvKLKNqms3pG6AutcO3e1';
+const CHANNEL_ID = '1107797248676483144'; //luca ch id
+const TELEGRAM_TOKEN = '6640266877:AAH0-fiYqVBCcTM5PrbIWEBYNuEUeAg0COo';
+const TELEGRAM_CHAT_ID = '1611894170';
 
 const limiter = new Bottleneck({
   minTime: 250, // 4 requests per second
 });
 
+async function notifyMissingAuthToken() {
+  const notificationMessage = "Missing or invalid DISCORD_AUTH. Please update the auth token.";
+  await sendMessageToTelegram(notificationMessage);
+  console.error(notificationMessage);
+  throw new Error(notificationMessage); // This will halt the execution of the app
+}
 async function hasYellowPixel(buffer) {
   const { data, info } = await sharp(buffer)
     .raw()
@@ -43,16 +48,50 @@ async function sendMessageToTelegram(text) {
   const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${text}`;
   await fetch(url);
 }
+
 // Create a Set to store processed message IDs
 const processedMessageIds = new Set();
 
 // Variable to hold the content of the previous message
 let prevMessageContent = '';
 
+async function obtainAccessToken() {
+  try {
+    const response = await axios.post('https://discord.com/api/v10/oauth2/token', null, {
+      params: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'client_credentials',
+        scope: 'messages.read'  // Adjust the scope according to your needs
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    
+    if (response.data && response.data.access_token) {
+      DISCORD_AUTH = `Bearer ${response.data.access_token}`;
+      console.log('Access token obtained successfully');
+      return true;
+    } else {
+      console.error('Failed to obtain access token');
+      return false;
+    }
+  } catch (error) {
+    console.error('Error obtaining access token:', error);
+    return false;
+  }
+}
+
 const checkForNewImageWithYellowPixel = async () => {
   try {
+    if (!DISCORD_AUTH) {
+      await notifyMissingAuthToken();
+      return;
+  }
+
     const response = await axios.get(
-      `https://discord.com/api/v9/channels/${CHANNEL_ID}/messages?limit=2`,
+      `https://discord.com/api/v10/channels/${CHANNEL_ID}/messages?limit=2`,
       {
         headers: { Authorization: DISCORD_AUTH },
       }
@@ -106,10 +145,18 @@ const checkForNewImageWithYellowPixel = async () => {
     }
   } catch (error) {
     console.error('Error fetching Discord messages:', error);
+    if (error.response && error.response.status === 401) {
+      console.log('Unauthorized - Access token may have expired or is invalid. Obtaining a new token...');
+      DISCORD_AUTH = '';
+      await checkForNewImageWithYellowPixel();
+    }
   }
 };
 
 const rateLimitedCheck = () => limiter.schedule(() => checkForNewImageWithYellowPixel());
 
-// Check for new messages with yellow pixels every 10 seconds
-setInterval(rateLimitedCheck, 10000);
+// Initially obtain the access token
+obtainAccessToken().then(() => {
+  // Check for new messages with yellow pixels every 10 seconds
+  setInterval(rateLimitedCheck, 10000);
+});
